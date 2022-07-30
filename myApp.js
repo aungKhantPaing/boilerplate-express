fb2f9539552bd2c103193b74ab59f3576984d5fa
aungKhantPaing <25960342+aungKhantPaing@users.noreply.github.com>
2022-07-31 00:17:43 +0630

    Basic Node and Express

--- myApp.js
+++ myApp.js
@@ -1,48 +1,53 @@
-let express = require('express');
+require("dotenv").config();
+const bodyParser = require("body-parser");
+let express = require("express");
 
 let app = express();
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/views/index.html')
+const logMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+};
+
+const addTimeMiddleware = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+app.use(logMiddleware);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
+
+app.get("/json", (_, res) => {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
+});
+
+app.get("/now", addTimeMiddleware, (req, res) => {
+  res.json({ time: req.time });
+});
+
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
+
+app
+  .route("/name")
+  .get((req, res) => {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+  })
+  .post((req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+  });
 
 console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
